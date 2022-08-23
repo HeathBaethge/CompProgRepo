ac38f6a901bad5dcf575e2fb3581417193f7695d
Heath <77910449+HeathBaethge@users.noreply.github.com>
2022-08-23 14:22:05 -0500

    C# project 1: Final Semester

---
+++ HBaethgeA2_1LL/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace HBaethgeA2_1LL
+{
+    partial class frmA2_1LL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbxDisplay = new System.Windows.Forms.ListBox();
+            this.btnGetItems = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxDisplay
+            // 
+            this.lbxDisplay.FormattingEnabled = true;
+            this.lbxDisplay.Location = new System.Drawing.Point(12, 12);
+            this.lbxDisplay.Name = "lbxDisplay";
+            this.lbxDisplay.Size = new System.Drawing.Size(193, 225);
+            this.lbxDisplay.TabIndex = 0;
+            // 
+            // btnGetItems
+            // 
+            this.btnGetItems.Location = new System.Drawing.Point(211, 41);
+            this.btnGetItems.Name = "btnGetItems";
+            this.btnGetItems.Size = new System.Drawing.Size(75, 23);
+            this.btnGetItems.TabIndex = 1;
+            this.btnGetItems.Text = "&Get Items";
+            this.btnGetItems.UseVisualStyleBackColor = true;
+            this.btnGetItems.Click += new System.EventHandler(this.btnGetItems_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(211, 70);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(211, 99);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "&Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(211, 128);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 23);
+            this.btnAverage.TabIndex = 4;
+            this.btnAverage.Text = "A&verage";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(211, 186);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(211, 157);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "&Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmA2_1LL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 251);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnGetItems);
+            this.Controls.Add(this.lbxDisplay);
+            this.Name = "frmA2_1LL";
+            this.Text = "Link List Project";
+            this.Load += new System.EventHandler(this.frmA2_1LL_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxDisplay;
+        private System.Windows.Forms.Button btnGetItems;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ HBaethgeA2_1LL/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HBaethgeA2_1LL
+{
+    public partial class frmA2_1LL : Form
+    {
+        LinkedList<double> numbers = new LinkedList<double>();
+
+        public frmA2_1LL()
+        {
+            InitializeComponent();
+        }
+
+        private void frmA2_1LL_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGetItems_Click(object sender, EventArgs e)
+        {
+            double[] random = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.10};
+            numbers = new LinkedList<double>(random);
+
+            foreach(double n in numbers)
+            {
+                lbxDisplay.Items.Add(n);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+            foreach (double n in numbers)
+            {
+                total += n;
+            }
+            lbxDisplay.Items.Add("Total of numbers in list is: " + total);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if(numbers.Remove(3.3))
+            {
+                foreach (double n in numbers)
+                {
+                    lbxDisplay.Items.Add(n);
+                }
+                lbxDisplay.Items.Add("Removed 3.3 from list.");
+            }
+            else
+            {
+                lbxDisplay.Items.Add("Number not found in list.");
+            }
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            double total = 0, count = 0;
+            foreach (double n in numbers)
+            {
+                total += n;
+                count++;
+            }
+            total /= count;
+            lbxDisplay.Items.Add("Average of numbers in list is: " + total);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbxDisplay.Items.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
