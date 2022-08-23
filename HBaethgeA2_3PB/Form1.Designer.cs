bda031e981347baef98825af5478d952f00e9503
Heath <77910449+HeathBaethge@users.noreply.github.com>
2022-08-23 14:25:39 -0500

    C# Project 3: Final Semester

---
+++ HBaethgeA2_3PB/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace HBaethgeA2_3PB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pgrProgress = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.bkgWorker = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // pgrProgress
+            // 
+            this.pgrProgress.Location = new System.Drawing.Point(40, 167);
+            this.pgrProgress.Name = "pgrProgress";
+            this.pgrProgress.Size = new System.Drawing.Size(486, 23);
+            this.pgrProgress.TabIndex = 0;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.AutoSize = true;
+            this.lblProgress.Location = new System.Drawing.Point(259, 202);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(35, 13);
+            this.lblProgress.TabIndex = 1;
+            this.lblProgress.Text = "label1";
+            // 
+            // bkgWorker
+            // 
+            this.bkgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bkgWorker_DoWork);
+            this.bkgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bkgWorker_ProgressChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 229);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.pgrProgress);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgrProgress;
+        private System.Windows.Forms.Label lblProgress;
+        private System.ComponentModel.BackgroundWorker bkgWorker;
+    }
+}
+
---
+++ HBaethgeA2_3PB/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace HBaethgeA2_3PB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bkgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for(int i = 1; i <=100; i++)
+            {
+                Thread.Sleep(50);
+                bkgWorker.ReportProgress(i);
+            }
+        }
+
+        private void bkgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pgrProgress.Value = e.ProgressPercentage;
+            lblProgress.Text = "Progress: " + e.ProgressPercentage.ToString() + "%";
+
+            if(pgrProgress.Value >= 100)
+            {
+                MessageBox.Show("Your application is open.");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bkgWorker.WorkerReportsProgress = true;
+            bkgWorker.RunWorkerAsync();
+        }
+    }
+}
